66918552b2cdcb60676a7f181aa45d09c3568999
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-01-25 17:52:40 -0500

    Squarify the world

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById('game');
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const size =
+  window.innerWidth < window.innerHeight
+    ? window.innerWidth
+    : window.innerHeight;
+
+canvas.width = size;
+canvas.height = size;
 
 const ctx = canvas.getContext('2d');
 
@@ -9,7 +14,7 @@ let x = 0;
 let y = 0;
 
 function update() {
-  ctx.clearRect(0, 0, width, height);
+  ctx.clearRect(0, 0, size, size);
   ctx.fillRect(x, y, 80, 75);
 
   x += Math.random() * 10 + 1;
