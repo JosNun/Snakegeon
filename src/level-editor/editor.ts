261066a08c59bed19d57d5c05e9cbdb0e75897bd
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-05-16 18:04:34 -0400

    Add tile selection

--- src/level-editor/editor.ts
+++ src/level-editor/editor.ts
@@ -1,4 +1,4 @@
-import { EntityType } from "../types";
+import { EntityType, LevelData } from "../types";
 
 let worldSize = 16;
 document.body.style.setProperty("--world-size", worldSize.toString());
@@ -7,6 +7,8 @@ let selectedEntity = EntityType.Wall;
 
 const toolPanel = document.querySelector(".tool-panel") as HTMLDivElement;
 const worldMap = document.querySelector(".world-map");
+
+let tiles: LevelData["tiles"] = [];
 
 const entityButtons = document.createElement("div");
 entityButtons.classList.add("entity-buttons");
@@ -21,6 +23,22 @@ function handleEntityTypeSelection(e: MouseEvent, type: EntityType) {
   selectedButton?.classList.remove("selected");
 
   (e.target as HTMLButtonElement | undefined)?.classList.add("selected");
+}
+
+function handleTileClick(e: MouseEvent, x: number, y: number) {
+  tiles = tiles.filter((tile) => {
+    return !(tile.x === x && tile.y === y);
+  });
+
+  tiles.push({
+    type: selectedEntity,
+    x,
+    y,
+  });
+
+  (e.target as HTMLDivElement).innerText = selectedEntity;
+
+  console.log(tiles);
 }
 
 Object.keys(EntityType).forEach((entity) => {
@@ -44,6 +62,10 @@ for (let i = 0; i < worldSize; i++) {
   for (let j = 0; j < worldSize; j++) {
     const tile = document.createElement("div");
     tile.classList.add("world-tile");
+
+    tile.addEventListener("click", (e) => {
+      handleTileClick(e, i, j);
+    });
 
     worldMap?.appendChild(tile);
   }
