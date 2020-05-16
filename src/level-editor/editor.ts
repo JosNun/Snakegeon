a9a96664ab09447a5d3bff9e81a805143e739034
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2020-05-16 17:58:40 -0400

    Add entity type selection

--- src/level-editor/editor.ts
+++ src/level-editor/editor.ts
@@ -3,6 +3,8 @@ import { EntityType } from "../types";
 let worldSize = 16;
 document.body.style.setProperty("--world-size", worldSize.toString());
 
+let selectedEntity = EntityType.Wall;
+
 const toolPanel = document.querySelector(".tool-panel") as HTMLDivElement;
 const worldMap = document.querySelector(".world-map");
 
@@ -11,10 +13,29 @@ entityButtons.classList.add("entity-buttons");
 
 toolPanel.appendChild(entityButtons);
 
+function handleEntityTypeSelection(e: MouseEvent, type: EntityType) {
+  selectedEntity = type;
+
+  const selectedButton = document.querySelector(".entity-button.selected");
+
+  selectedButton?.classList.remove("selected");
+
+  (e.target as HTMLButtonElement | undefined)?.classList.add("selected");
+}
+
 Object.keys(EntityType).forEach((entity) => {
   const btn = document.createElement("button");
+  const type: EntityType = EntityType[
+    (entity as any) as keyof typeof EntityType
+  ] as any;
+
   btn.innerText = entity;
   btn.classList.add("entity-button");
+  if (type === selectedEntity) {
+    btn.classList.add("selected");
+  }
+
+  btn.onclick = (e) => handleEntityTypeSelection(e, type);
 
   entityButtons.appendChild(btn);
 });
